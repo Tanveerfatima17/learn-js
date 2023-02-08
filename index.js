cb3ccdb2d3757b4eb2aec1f1e58a7375f23c92b6
Tanveer <thungund@gmail.com>
2023-02-08 23:04:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// console.log is used to display text in the console
+/*console.log("Hello world!")
+console.log('Tanveer here')
+
+//window.alert creates a pop up on your webpage
+window.alert("Wow I am learning javascript!")
+
+// var or let keyword is used to declare a variable
+let age=19
+let name1="Tanveer"
+
+console.log(age)
+//write in paragraph using id 
+document.getElementById("p1").innerHTML = "Hello "+ name1;
+
+//how to take input from the users
+//1. easy way using window prompt
+let username=window.prompt("What's your name: ")
+console.log(username)
+
+//with html textbox
+let username2;
+document.getElementById("button1").onclick=function(){
+username2=document.getElementById("text1").value
+console.log(username2)
+document.getElementById("lab1").innerHTML = "Hello "+username2
+
+}
+let age=window.prompt("Whats your age?")
+console.log(typeof age)
+age=Number(age)
+age=age+1
+console.log("You are "+age+" years old now!")*/
+
+function sayHello(name){
+    document.write("Hello there!!")
+}
+
+
+
